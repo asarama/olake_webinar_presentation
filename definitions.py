e1064d27742b26faa87b79ec594a67e42faa7cb9
asarama <arshameslami@hotmail.com>
2025-09-04 00:13:50 -0700

    Some env var updates

--- definitions.py
+++ definitions.py
@@ -3,14 +3,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
     S3_BUCKET_NAME: str
-    AWS_S3_ACCESS_KEY_ID: str
+    AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
     AWS_S3_REGION: str = "us-west-2"
+    LAKEKEEPER_CATALOG_URL: str
+    LAKEKEEPER_WAREHOUSE: str
 
 
 settings = Settings.model_validate({})
 
 S3_BUCKET_NAME = settings.S3_BUCKET_NAME
-AWS_S3_ACCESS_KEY_ID = settings.AWS_S3_ACCESS_KEY_ID
+AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
 AWS_S3_REGION = settings.AWS_S3_REGION
+
+LAKEKEEPER_CATALOG_URL = settings.LAKEKEEPER_CATALOG_URL
+LAKEKEEPER_WAREHOUSE = settings.LAKEKEEPER_WAREHOUSE
+
--- lakekeeper-clear.py
+++ lakekeeper-clear.py
@@ -13,11 +13,11 @@ lakekeeper_catalog = load_catalog(
 )
 timer.add("load_catalog")
 
-lakekeeper_catalog.create_namespace_if_not_exists("hello_world")
-timer.add("create_namespace")
+lakekeeper_catalog.drop_namespace("hello_world")
+timer.add("drop_namespace")
 
 
-lakekeeper_catalog.create_namespace_if_not_exists("hello_world_2")
-timer.add("create_namespace_2")
+lakekeeper_catalog.drop_namespace("hello_world_2")
+timer.add("drop_namespace_2")
 
 timer.display()
