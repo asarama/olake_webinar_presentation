657796c900349b1be5d1612025ca002253d8cc9e
asarama <arshameslami@hotmail.com>
2025-09-03 22:58:54 -0700

    Add some env vars

---
+++ definitions.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    S3_BUCKET_NAME: str
+    AWS_S3_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_S3_REGION: str = "us-west-2"
+
+
+settings = Settings.model_validate({})
+
+S3_BUCKET_NAME = settings.S3_BUCKET_NAME
+AWS_S3_ACCESS_KEY_ID = settings.AWS_S3_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
+AWS_S3_REGION = settings.AWS_S3_REGION
